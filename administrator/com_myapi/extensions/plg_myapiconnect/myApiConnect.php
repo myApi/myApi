e8b45bc29d29563f95d2db339dadd1c6938d0083
Thomas Welton <thomaswelton@me.com>
2011-04-05 14:15:44 +0100

    Added a locale setting to the myApiConnect system plugin. This allows admins the ability to easily change the language of the social plugins.

--- administrator/com_myapi/extensions/plg_myapiconnect/myApiConnect.php
+++ administrator/com_myapi/extensions/plg_myapiconnect/myApiConnect.php
@@ -76,7 +76,7 @@ class plgSystemmyApiConnect extends JPlugin
   (function() {
     var e = document.createElement('script'); e.async = true;
     e.src = document.location.protocol +
-      '//connect.facebook.net/en_US/all.js';
+      '//connect.facebook.net/{$com_params->get("locale")}/all.js';
     document.getElementById('fb-root').appendChild(e);
   }());
   });
